f87de6290bf199a64722d4abb34f1d16eda7ad9e
KZTahat <kztahat96@gmail.com>
2021-07-31 17:51:29 +0300

    setting up for netlify

--- src/Components/FirstLayer/Products.js
+++ src/Components/FirstLayer/Products.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 import { Card, Button } from "react-bootstrap";
 import "./firstLayer.css";
-import {withAuth0} from '@auth0/auth0-react';
+import { withAuth0 } from "@auth0/auth0-react";
 
 class Products extends React.Component {
   constructor(props) {
@@ -12,7 +12,7 @@ class Products extends React.Component {
 
   componentDidMount = async () => {
     const isAuthenticated = this.props.auth0.isAuthenticated;
-    console.log('Products', isAuthenticated);
+    console.log("Products", isAuthenticated);
     let response = [];
     try {
       response = await axios.get(`${process.env.REACT_APP_SERVER}/getlists`);
@@ -25,33 +25,35 @@ class Products extends React.Component {
   };
 
   render() {
-    return (
-      <>
-        {this.state.products.map((element, index) => {
-          return (
-            <div key={index} className="cardProductDiv">
-              <Card style={{ width: "18rem" }}>
-                <Card.Img
-                  variant="top"
-                  src={`https://image.tmdb.org/t/p/w500/${element.poster_path}`}
-                />
-                <Card.Body>
-                  <Card.Title>{element.original_title}</Card.Title>
-                  <Card.Text>{element.overview}</Card.Text>
-                  <Card.Text>Price: {element.price} $</Card.Text>
-                  <Button
-                    onClick={() => this.props.addToCart(index, element)}
-                    variant="primary"
-                  >
-                    Add To Cart
-                  </Button>
-                </Card.Body>
-              </Card>
-            </div>
-          );
-        })}
-      </>
-    );
+    if (this.state.products != null) {
+      return (
+        <>
+          {this.state.products.map((element, index) => {
+            return (
+              <div key={index} className="cardProductDiv">
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src={`https://image.tmdb.org/t/p/w500/${element.poster_path}`}
+                  />
+                  <Card.Body>
+                    <Card.Title>{element.original_title}</Card.Title>
+                    <Card.Text>{element.overview}</Card.Text>
+                    <Card.Text>Price: {element.price} $</Card.Text>
+                    <Button
+                      onClick={() => this.props.addToCart(index, element)}
+                      variant="primary"
+                    >
+                      Add To Cart
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            );
+          })}
+        </>
+      );
+    }else {return <h4 style={{marginTop:'100px'}}>Your movies List is unreachable</h4>}
   }
 }
 
